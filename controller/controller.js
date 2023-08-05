1f60df29181fb8cd0c062b314439136fe36888ca
TranVanTienDat <ckim32993@gmail.com>
2023-08-05 17:41:29 +0700

    a

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,6 @@
 const userDB = require("../models/model");
 const jwt = require("jsonwebtoken");
-const argon2 = require("argon2");
+const bcrypt = require("bcryptjs");
 const e = require("express");
 
 // User registration
@@ -47,7 +47,6 @@ const e = require("express");
 //       });
 //   });
 // };
-
 exports.create = (req, res) => {
   if (!req.body) {
     res.status(400).send({ message: "not found" });
@@ -67,9 +66,16 @@ exports.create = (req, res) => {
     }
 
     const saltRounds = 10;
-    argon2
-      .hash(password, saltRounds)
-      .then((hashedPassword) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ message: "error" });
+      }
+      bcrypt.hash(password, salt, (err, hashedPassword) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send({ message: "error" });
+        }
         const user = new userDB({
           name,
           email,
@@ -84,165 +90,9 @@ exports.create = (req, res) => {
             res.status(200).json(data);
           })
           .catch((err) => res.status(500).send({ message: "error" }));
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send({ message: "error" });
       });
+    });
   });
-};
-
-//Take out all users
-// exports.get = (req, res) => {
-//   if (req.query.id) {
-//     const id = req.query.id;
-
-//     userDB
-//       .findById(id)
-//       .then((data) => {
-//         if (!data) {
-//           res.status(404).send({ message: "Not found user with id " + id });
-//         } else {
-//           res.send(data);
-//         }
-//       })
-//       .catch((err) => {
-//         res
-//           .status(500)
-//           .send({ message: "Error retrieving user with id " + id });
-//       });
-//   } else {
-//     userDB
-//       .find()
-//       .then((user) => {
-//         res.send(user);
-//       })
-//       .catch((err) => {
-//         res.status(500).send({
-//           message:
-//             err.message || "Error Occurred while retrieving user information",
-//         });
-//       });
-//   }
-// };
-
-exports.get = (req, res) => {
-  if (req.query.id) {
-    const id = req.query.id;
-
-    userDB
-      .findById(id)
-      .then((data) => {
-        if (!data) {
-          res.status(404).send({ message: "Not found user with id " + id });
-        } else {
-          // Send the user data without password
-          const { password, ...userData } = data._doc;
-          res.send(userData);
-        }
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .send({ message: "Error retrieving user with id " + id });
-      });
-  } else {
-    userDB
-      .find()
-      .then((users) => {
-        // Exclude passwords from the response
-        const filteredUsers = users.map((user) => {
-          const { password, ...userData } = user._doc;
-          return userData;
-        });
-
-        res.send(filteredUsers);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message:
-            err.message || "Error occurred while retrieving user information",
-        });
-      });
-  }
-};
-
-//Update user
-// exports.update = (req, res) => {
-//   if (!req.body) {
-//     res.status(400).send({ message: "error" });
-//   }
-
-//   const id = req.params.id;
-
-//   userDB
-//     .findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-//     .then((data) => {
-//       if (!data) {
-//         res.status(404).send({ message: `update isn't success for ${id}` });
-//       } else {
-//         res.send(data);
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).send({ message: "error update" });
-//     });
-// };
-
-exports.update = (req, res) => {
-  if (!req.body) {
-    res.status(400).send({ message: "error" });
-  }
-
-  const id = req.params.id;
-
-  // Exclude password field from the update data
-  const { password, ...updateData } = req.body;
-
-  // Only hash the password if it is provided in the update data
-  if (password) {
-    // Hash the password using argon2
-    argon2
-      .hash(password)
-      .then((hashedPassword) => {
-        // Update the updateData object with the hashed password
-        updateData.password = hashedPassword;
-
-        userDB
-          .findByIdAndUpdate(id, updateData, { useFindAndModify: false })
-          .then((data) => {
-            if (!data) {
-              res
-                .status(404)
-                .send({ message: `Update isn't successful for ${id}` });
-            } else {
-              res.send(data);
-            }
-          })
-          .catch((err) => {
-            res.status(500).send({ message: "Error updating user" });
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send({ message: "Error hashing password" });
-      });
-  } else {
-    userDB
-      .findByIdAndUpdate(id, updateData, { useFindAndModify: false })
-      .then((data) => {
-        if (!data) {
-          res
-            .status(404)
-            .send({ message: `Update isn't successful for ${id}` });
-        } else {
-          res.send(data);
-        }
-      })
-      .catch((err) => {
-        res.status(500).send({ message: "Error updating user" });
-      });
-  }
 };
 
 // Login users
@@ -285,27 +135,79 @@ exports.login = (req, res) => {
         return res.status(404).send({ message: "Email not registered." });
       }
 
-      argon2
-        .verify(foundUser.password, req.body.password)
-        .then((match) => {
-          if (!match) {
-            return res.status(401).send({ message: "Incorrect password" });
-          }
+      bcrypt.compare(req.body.password, foundUser.password, (err, match) => {
+        if (err) {
+          return res.status(500).send({ message: "Internal server error" });
+        }
+        if (!match) {
+          return res.status(401).send({ message: "Incorrect password" });
+        }
 
-          const token = jwt.sign(
-            { id: foundUser._id },
-            process.env.ACCESS_TOKEN_SECRET
-          );
-          res
-            .status(200)
-            .send({ auth: true, token: token, message: "Success" });
-        })
-        .catch((error) => {
-          res.status(500).send({ message: "Internal server error" });
-        });
+        const token = jwt.sign(
+          { id: foundUser._id },
+          process.env.ACCESS_TOKEN_SECRET
+        );
+        res.status(200).send({ auth: true, token: token, message: "Success" });
+      });
     })
     .catch((error) => {
       res.status(500).send({ message: "Internal server error" });
+    });
+};
+
+//Take out all users
+exports.get = (req, res) => {
+  if (req.query.id) {
+    const id = req.query.id;
+
+    userDB
+      .findById(id)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({ message: "Not found user with id " + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving user with id " + id });
+      });
+  } else {
+    userDB
+      .find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "Error Occurred while retrieving user information",
+        });
+      });
+  }
+};
+
+//Update user
+exports.update = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "error" });
+  }
+
+  const id = req.params.id;
+
+  userDB
+    .findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `update isn't success for ${id}` });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "error update" });
     });
 };
 
@@ -399,7 +301,6 @@ exports.getUserData = (req, res) => {
 //       res.status(500).send({ message: "Internal server error" });
 //     });
 // };
-
 exports.updatePassword = (req, res) => {
   if (!req.body) {
     return res.status(400).send({ message: "error" });
@@ -417,20 +318,17 @@ exports.updatePassword = (req, res) => {
           .send({ message: `User not found with id ${id}` });
       }
 
-      argon2
-        .verify(foundUser.password, currentPassword)
-        .then((match) => {
-          if (!match) {
-            return res
-              .status(401)
-              .send({ message: "Incorrect current password" });
-          }
-
+      bcrypt.compare(currentPassword, foundUser.password, (err, match) => {
+        if (err) {
+          res.status(500).send({ message: "Internal server error" });
+        } else if (!match) {
+          res.status(401).send({ message: "Incorrect current password" });
+        } else {
           const saltRounds = 10;
-
-          argon2
-            .hash(newPassword, saltRounds)
-            .then((hashedPassword) => {
+          bcrypt.hash(newPassword, saltRounds, (err, hashedPassword) => {
+            if (err) {
+              res.status(500).send({ message: "Error" });
+            } else {
               userDB
                 .findByIdAndUpdate(
                   id,
@@ -449,14 +347,10 @@ exports.updatePassword = (req, res) => {
                 .catch((err) => {
                   res.status(500).send({ message: "Error updating password" });
                 });
-            })
-            .catch((err) => {
-              res.status(500).send({ message: "Error" });
-            });
-        })
-        .catch((error) => {
-          res.status(500).send({ message: "Internal server error" });
-        });
+            }
+          });
+        }
+      });
     })
     .catch((error) => {
       res.status(500).send({ message: "Internal server error" });
@@ -510,14 +404,19 @@ exports.forgotPassword = (req, res) => {
       return res.status(404).send({ message: "Email not registered." });
     }
 
-    //  Generate new password
+    // Generate new password
     const newPassword = Math.random().toString(36).substring(2, 8);
 
     // Hash new password and update in db
     const saltRounds = 10;
-    argon2
-      .hash(newPassword, { salt: saltRounds })
-      .then((hashedPassword) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return res.status(500).send({ message: "Internal server error" });
+      }
+      bcrypt.hash(newPassword, salt, (err, hashedPassword) => {
+        if (err) {
+          return res.status(500).send({ message: "Internal server error" });
+        }
         userDB
           .findByIdAndUpdate(
             foundUser._id,
@@ -526,17 +425,15 @@ exports.forgotPassword = (req, res) => {
           )
           .then(() => {
             // Return new password to user
-            res
-              .status(200)
-              .send({ message: `Your new password is: ${newPassword}` });
+            res.status(200).send({
+              message: `Your new password is: ${newPassword}`,
+            });
           })
           .catch((error) => {
             res.status(500).send({ message: "Internal server error" });
           });
-      })
-      .catch((error) => {
-        res.status(500).send({ message: "Internal server error" });
       });
+    });
   });
 };
 
